79448a54b6f251c55f402dc64c841affa4497dfe
Louise Au Yeung <louise@eabx.org>
2013-10-09 10:03:04 -0400

    initcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeQuery::Application.config.secret_key_base = '0aea8c66cdcb006e94292ca080a28bf4fcf362e4cb337c0e5036bdb8f06809ae6fa67a8145994555c7f289ce2519d874d1cc366ef451b2bcad2ca67071c10b1f'
