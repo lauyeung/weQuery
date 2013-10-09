7c8a749ff45101d8a50c5f245ca0b4ed8e201253
Louise Au Yeung <louise@eabx.org>
2013-10-09 15:24:27 -0400

    have random usernames per session

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe User do
-  
+describe User, focus: true do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it 'should anonymize my name!!!' do
+    anonymize = user.anonymizer
+    expect(user.anonymizer).to_not eql(anonymize)
+  end
+
 end
