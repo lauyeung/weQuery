ec5ef0b124fd8d2c15d49bf572336a7a6b7181f9
Louise Au Yeung <louise@eabx.org>
2013-10-09 14:15:02 -0400

    added relationship between users and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  belongs_to :user,
+    inverse_of: :questions
 
   validates_presence_of :content
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009155611) do
+ActiveRecord::Schema.define(version: 20131009181324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131009155611) do
     t.string   "content",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",    null: false
   end
 
   create_table "users", force: true do |t|
