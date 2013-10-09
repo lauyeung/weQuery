e1e57cddcaf176b12ed755181af9829d85ac5d37
Ashley Basinger <ashley.hipps@gmail.com>
2013-10-09 11:39:06 -0400

    added first question spec

---
+++ spec/features/user_adds_a_question_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'user submits a question' %Q{
+  As a launch academy student
+  I want to submit a question
+  So that my instructor will know what questions people have about the material and what topics need clarification
+
+  # ACCEPTANCE CRITERIA
+  # * I will be able to enter a question or topic for clarification
+
+} do
+
+  scenario 'user submits a question' do
+    user = FactoryGirl.create(:user)
+    prev_count = Question.count
+
+    sign_in_as(user)
+
+    visit '/'
+    fill_in 'Question', with "What is going on?"
+    click_button 'Submit'
+
+    expect(page).to have_content('Question successfully posted')
+    expect(page).to have_content('What is going on?')
+    expect(Question.count).to eql(prev_count + 1)
+  end 
