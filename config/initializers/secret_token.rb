4515d3dcf0c6e3d641b7c614e07eb2c72a08cbee
Louise Au Yeung <louise@eabx.org>
2013-10-09 10:12:41 -0400

    updated secret_token initializer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-WeQuery::Application.config.secret_key_base = '0aea8c66cdcb006e94292ca080a28bf4fcf362e4cb337c0e5036bdb8f06809ae6fa67a8145994555c7f289ce2519d874d1cc366ef451b2bcad2ca67071c10b1f'
+WeQuery::Application.config.secret_key_base = ENV['SECRET_TOKEN']
