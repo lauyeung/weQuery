9dede19c52bfe6a9f6bbeb1bb50b4b7a60ef56ec
ashleytbasinger <ashley.hipps@gmail.com>
2013-10-10 14:37:02 -0400

    added a few more things to the name arrays

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,21 +20,21 @@ class User < ActiveRecord::Base
       adjectives = [
       "boiling", "hidden", "bitter", "misty", "loquacious", "empty", "dry", "dark",
       "rick-rolled", "icy", "cryptic", "quiet", "caffeinated", "cool", "radiant", "electric",
-      "patient", "superfluous", "dawn", "crimson", "furious", "dry", "blue",
-      "billowing", "broken", "cold", "bearded", "falling", "frosty", "green",
-      "long", "late", "lingering", "bold", "little", "morning", "muddy", "blazing",
-      "red", "rough", "feisty", "small", "sparkling", "throbbing", "shy",
+      "patient", "superfluous", "dawn", "crimson", "furious", "fresh", "blue",
+      "billowing", "broken", "pumpkin", "bearded", "falling", "frosty", "green",
+      "long", "late", "lingering", "bold", "little", "morning", "glorious", "blazing",
+      "red", "rough", "feisty", "delicious", "sparkling", "throbbing", "shy",
       "wandering", "emo", "wild", "black", "screaming", "falling", "solitary",
       "fragrant", "shielded", "snowy", "proud", "agile", "twerking", "thirsty",
       "Vulcan", "ancient", "green", "lively", "nameless"
     ]
 
     nouns = [
-      "waterfall", "river", "breeze", "ninja", "wizard", "rain", "sea", "astronaut",
+      "waterfall", "river", "donuts", "ninja", "wizard", "rain", "Adam's-Beard", "astronaut",
       "Weasley", "yeti", "sunset", "pine", "shadow", "leaf", "dawn", "shoes", "tribble",
       "forest", "harbor", "cloud", "jedi", "sandwich", "glade", "eagles", "brook",
       "butterfly", "ocean", "dew", "kitten", "dumpling", "fire", "launcher", "firefly",
-      "feather", "REDBULL", "haze", "mountain", "night", "elf", "darkness",
+      "sweet-potato", "REDBULL", "haze", "mountain", "night", "elf", "darkness",
       "fjord", "silence", "reef", "hobbit", "shape", "surf", "thunder",
       "citadel", "water", "shwag", "dragon", "water", "resonance", "sword",
       "wood", "bayou", "cherry", "tree", "MajorTom", "frost", "crocodile", "octocat",
