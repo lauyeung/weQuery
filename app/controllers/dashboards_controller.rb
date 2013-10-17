bab8f4f090d14e6ffadd871f2085b2f05f3cada3
Louise Au Yeung <louise@eabx.org>
2013-10-17 08:30:55 -0400

    updated order of questions on index pages

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class DashboardsController < ApplicationController
 
   def index
-    @important_questions = Question.where(state: 'important').order("votes_count DESC")
-    @sent_questions = Question.where(state: 'sent').order("votes_count DESC")
-    @asked_questions = Question.where(state: 'asked').order("votes_count DESC")
+    @important_questions = Question.where(state: 'important').order("votes_count DESC, created_at DESC")
+    @sent_questions = Question.where(state: 'sent').order("votes_count DESC, created_at DESC")
+    @asked_questions = Question.where(state: 'asked').order("votes_count DESC, created_at DESC")
   end
 
 end
