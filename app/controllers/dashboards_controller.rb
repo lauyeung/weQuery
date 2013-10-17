b98ab18b56fae7c27179d298690bc7b371d1ab1b
Louise Au Yeung <louise@eabx.org>
2013-10-17 08:33:13 -0400

    reordered questions on index pages

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class DashboardsController < ApplicationController
 
   def index
-    @important_questions = Question.where(state: 'important').order("votes_count DESC, created_at DESC")
-    @sent_questions = Question.where(state: 'sent').order("votes_count DESC, created_at DESC")
-    @asked_questions = Question.where(state: 'asked').order("votes_count DESC, created_at DESC")
+    @important_questions = Question.where(state: 'important').order("votes_count DESC, created_at")
+    @sent_questions = Question.where(state: 'sent').order("votes_count DESC, created_at")
+    @asked_questions = Question.where(state: 'asked').order("votes_count DESC, created_at")
   end
 
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   def index
     Question.check_expiration
     @question = Question.new
-    @questions = Question.where("state != ?", "expired").order("votes_count DESC, created_at DESC")
+    @questions = Question.where("state != ?", "expired").order("votes_count DESC, created_at")
 
   end
 
